d0980ac82c6a7a39c32a8a3e8009ac2476dc3bf0
MichaelAndresCoronado <macoronado@espe.edu.ec>
2026-05-10 22:39:34 -0500

    feat: microservicio ms-flota-rest operativo y config del pipeline telegram

--- ms-flota-rest/src/main/java/ec/edu/espe/logiflow/flota/MsFlotaRestApplication.java
+++ ms-flota-rest/src/main/java/ec/edu/espe/logiflow/flota/MsFlotaRestApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MsFlotaRestApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MsFlotaRestApplication.class, args);
 	}
-
 }
